0da03ee5f83e4c0777ce46357713241b8f68eeaf
Derek Bassett <derek.bassett@protonmail.com>
2018-11-08 14:38:10 -0700

    Initial Commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("what???")
+	}
+}
+
+func run() {
+	fmt.Printf("running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+	// Setup Stdin, Stdout, Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+}
+
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
